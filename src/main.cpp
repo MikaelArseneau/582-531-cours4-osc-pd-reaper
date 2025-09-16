f4ff46bb629110a671996807f4ed4405c6e774ce
MikaelArseneau <mikael.arseneau@hotmail.ca>
2025-09-16 17:34:09 -0400

    pret pour exam

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,34 @@
 #include <Arduino.h>
+#include <MicroOscSlip.h>
+#include <FastLED.h>
+// Le nombre 128 entre les < > ci-dessous est le nombre maximal d'octets réservés pour les messages entrants.
+// Les messages sortants sont écrits directement sur la sortie et ne nécessitent pas de réservation d'octets supplémentaires.
+MicroOscSlip<128> monOsc(&Serial);
 
-// put function declarations here:
-int myFunction(int, int);
+
+#define MA_BROCHE_BOUTON 39
+#define MA_BROCHE_ANGLE 32
+
+
+CRGB monPixel;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  pinMode( MA_BROCHE_BOUTON , INPUT );
+  FastLED.addLeds<WS2812, 27, GRB>(&monPixel, 1);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  int maLectureBouton = digitalRead( MA_BROCHE_BOUTON );
+  int lectureAnalogique = analogRead(MA_BROCHE_ANGLE);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  
+monOsc.sendInt( "/bouton" , maLectureBouton);
+monOsc.sendInt( "/angle" , lectureAnalogique);
+
+monPixel = CRGB(0, (millis()%255), 0);
+FastLED.show();
+
+delay(100);
 }
